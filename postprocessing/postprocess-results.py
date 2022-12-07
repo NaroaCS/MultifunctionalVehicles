e85673c86d0607d5d4214c1cf860f7f813f7b9aa
NaroaCS <naroa.coretti@gmail.com>
2022-12-07 11:15:00 -0500

    numBikes range

--- postprocessing/postprocess-results.py
+++ postprocessing/postprocess-results.py
@@ -5,7 +5,7 @@ import glob
 
 #CHANGE folder  
 
-os.chdir("../results/2022-12-06 05.00.00")
+os.chdir("../results/2022-12-06 04.30.00")
 
 
 if True: #Load all csv files in directory and concat just once 
